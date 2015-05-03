665dfb7e250eb29e49ec0bef0ba58aac06a4dfb5
William O'Connor <woconnor@smu.edu>
2015-05-03 11:55:10 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelpMeText::Application.config.secret_key_base = '5fa5673459e66e92ce49295e8bd956461a99fa27fa55d28c6855543095a88443dbe0b2a832cc5fb4049fa4b775b1555ae5269418a276bafcde13fd4303d05f06'
---
+++ db/migrate/20150503163713_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.integer :age
+      t.string :gender
+      t.string :interest
+
+      t.timestamps
+    end
+  end
+end
